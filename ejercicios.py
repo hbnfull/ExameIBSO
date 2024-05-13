66f1337dd1558782a881f7af100077f166314686
Daniel Sanchez <dasd2405@gmail.com>
2024-05-13 09:52:12 -0600

    correccion de ejercicio 2 no guardado

--- ejercicios.py
+++ ejercicios.py
@@ -16,37 +16,3 @@ for clave in diccionario:
     print(clave, "promedio", total/7, "mayor", mayor, "menor", menor)
 
 print("Ciudad con menos temperatura: " + ciudadBaja + " | Ciudad con mas temperatura: " + ciudadAlta)
-
-def obtener_valor(letra):
-    # Obtener el valor de la letra en función de su posición en el alfabeto
-    return ord(letra) - ord('a') + 1
-
-def calcular_suma(cadena):
-    suma = 0
-    for letra in cadena:
-        if letra.islower():
-            suma += obtener_valor(letra)
-        else:
-            print(f"Error: La letra '{letra}' no es minúscula.")
-            return None
-    return suma
-
-def pedir_entrada():
-    cadena = input("Ingrese una cadena de letras minúsculas: ")
-    if not cadena.isalpha():
-        for i, caracter in enumerate(cadena, 1):
-            if not caracter.islower():
-                print(f"Cambia por una letra el numero '{caracter}' en la posición {i}.")
-                return None
-    for i, caracter in enumerate(cadena, 1):
-            if not caracter.islower():
-                print(f"Cambia a minúscula la letra '{caracter}' en la posición {i}.")
-                return None
-    return cadena
-
-cadena = pedir_entrada()
-while cadena is None:
-    cadena = pedir_entrada()
-suma = calcular_suma(cadena)
-if suma is not None:
-    print(f"La suma de los valores de las letras en la cadena es: {suma}")
